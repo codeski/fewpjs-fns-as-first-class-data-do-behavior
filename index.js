86b3c1b4f44419c7a275849b16fda451fc82bc68
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-03-16 13:59:14 -0600

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  const decimal = (time.split(":").join("."))
+  const integer = parseInt(decimal)
+  if (time === "") {
+    return "'TEST'"
+  } else if (integer < 12) {
+    return "Good Morning"
+  } else if (integer < 17) {
+    return "Good Afternoon"
+  } else if (integer > 17) {
+    return "Good Evening"
+  } else if (time) {return `'${time}'`}
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  const addToGreeting = document.getElementById('greeting')
+  addToGreeting.innerHTML = string
+}
